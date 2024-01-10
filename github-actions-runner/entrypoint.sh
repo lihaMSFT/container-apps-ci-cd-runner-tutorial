7ca191ea438354fc4e42e9ede825b558409619af
lihaMSFT <liha@microsoft.com>
2024-01-09 19:41:37 -0800

    Update entrypoint.sh

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -8,4 +8,4 @@ REGISTRATION_TOKEN="$(curl -X POST -fsSL \
   "$REGISTRATION_TOKEN_API_URL" \
   | jq -r '.token')"
 
-./config.sh --url $REPO_URL --token $REGISTRATION_TOKEN --unattended --disableupdate --ephemeral --replace && ./run.sh
+./config.sh --url $REPO_URL --token $REGISTRATION_TOKEN --unattended --ephemeral --replace && ./run.sh
